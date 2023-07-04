5298f0cbde37636ba48e619ba640ff6140665465
VigalOculus33 <vladoffski_sv@mail.ru>
2023-07-05 00:10:07 +0500

    решаем задачу 55

---
+++ Seminar8/Zadacha55/Program.cs
@@ -0,0 +1,19 @@
+﻿
+const int ROWS = 3;
+const int COLUMNS = 4;
+
+int[,] GetRandomMatryx(int rows,int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for(int i = 0;i < matrix.GetLength(0);i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = Random.Shared.Next(1,10);
+        }
+    }
+    return matrix;
+}
+
+int[,] myMatrix = GetRandomMatryx(ROWS, COLUMNS);
+Console.W

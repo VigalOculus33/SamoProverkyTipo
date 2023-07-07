349823495890a4e9a4e30b59f79de0cafb26e942
VigalOculus33 <vladoffski_sv@mail.ru>
2023-07-07 15:48:30 +0500

    решили с божей помощью задачу 50

--- Zadacha47/Program.cs
+++ Zadacha47/Program.cs
@@ -1,14 +1,18 @@
-﻿
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
 
 
 const int Ranks = 3;
-const int Posts = 5;
+const int Posts = 4;
 double[,] TakeRandomMatrix(int ranks, int posts){
 double[,] matrix = new double[ranks, posts];  
 for(int i = 0; i < matrix.GetLength(0); i++){
     for(int j = 0; j < matrix.GetLength(1); j++)
     {
-        matrix[i, j] = Random.Shared.Next(1, 10);
+        matrix[i, j] = Random.Shared.Next(1,10);
     }
 }
 return matrix;
---
+++ Zadacha50/Program.cs
@@ -0,0 +1,38 @@
+﻿
+const int Ranks = 3;
+const int Posts = 4;
+int[,] TakeRandomMatrix(int ranks, int posts)
+{
+int[,] matrix = new int[ranks, posts];  
+for(int i = 0; i < matrix.GetLength(0); i++){
+    for(int j = 0; j < matrix.GetLength(1); j++){
+        matrix[i, j] = Random.Shared.Next(1, 10);
+    }
+}
+return matrix;
+}
+void PrintlnMatrix(int[,] matrix){
+for(int i = 0; i < matrix.GetLength(0); i++){
+
+    for(int j = 0; j < matrix.GetLength(1); j++){
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите строку :");
+int ranks = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Введите стобец :");
+int posts = Convert.ToInt32(Console.ReadLine()); 
+
+int[,] myMatrix = TakeRandomMatrix(Ranks, Posts);
+PrintlnMatrix(myMatrix);
+
+if (ranks >= 0 && ranks < myMatrix.GetLength(0) && posts >= 0 && posts < myMatrix.GetLength(1))
+{
+ int element = myMatrix[ranks, posts];
+ Console.WriteLine($"Значение элемента: {element}");
+} else {
+   Console.WriteLine("Срочно санитаров!!!");
+    }

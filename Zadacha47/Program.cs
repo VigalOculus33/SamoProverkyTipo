5db7667d99099d234a5be990884a46678d6b6bd6
VigalOculus33 <vladoffski_sv@mail.ru>
2023-07-07 22:36:08 +0500

    решили с божей помощью задачу 47

--- Zadacha47/Program.cs
+++ Zadacha47/Program.cs
@@ -12,7 +12,7 @@ double[,] matrix = new double[ranks, posts];
 for(int i = 0; i < matrix.GetLength(0); i++){
     for(int j = 0; j < matrix.GetLength(1); j++)
     {
-        matrix[i, j] = Random.Shared.Next(1,10);
+        matrix[i, j] = Random.Shared.NextDouble(1,10);
     }
 }
 return matrix;

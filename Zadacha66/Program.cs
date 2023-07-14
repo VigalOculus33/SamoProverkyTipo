59b97b338bcab05671317e8045037e0e11267080
VigalOculus33 <vladoffski_sv@mail.ru>
2023-07-14 16:01:01 +0500

    решили домашку семинара

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+//Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+class Program
+{
+static Dictionary<string, int> memo = new Dictionary<string, int>();
+
+    static void Main(string[] args)
+    {
+        int m = 2;
+        int n = 3;
+
+        int result = Ackermann(m, n);
+        Console.WriteLine("A({0}, {1}) = {2}", m, n, result);
+    }
+
+    static int Ackermann(int m, int n)
+    {
+        // Проверка на отрицательные значения m и n
+        if (m < 0 || n < 0)
+        {
+            throw new ArgumentException("m и n должны быть неотрицательными целыми числами.");
+        }
+
+        // Проверка на максимальные значения переменных m и n
+        if (m > 3)
+        {
+            throw new ArgumentException(" }{Yli tak mnogo dl`a Ackermann function .");
+        }
+        int result;
+
+        if (m == 0)
+        {
+            result = n + 1;
+        }
+        else if (m > 0 && n == 0)
+        {
+            result = Ackermann(m - 1, 1);
+        }
+        else
+        {
+            result = Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+        return result;
+    }
+}
---
+++ Zadacha68/Program.cs
@@ -0,0 +1,9 @@
+﻿// в JavaRush решал задачи через тернальный оператор и получилось и на шарпе)))))))))))
+int m = 2;
+int n = 3;
+int Ackermann(int m, int n)
+{
+return m == 0 ? n + 1 : (m > 0 && n == 0) ? Ackermann(m - 1, 1) : Ackermann(m - 1, Ackermann(m, n - 1));
+}
+int result = Ackermann(m, n);
+Console.WriteLine("A({0}, {1}) = {2}", m, n, result);
